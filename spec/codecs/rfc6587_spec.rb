ThePsyjo/logstash-codec-rfc6587

commit f480cf92ce03ce9245981e2e7be29aeb9e7b64a7

fix: fix newline handling pointed out in #2
Added a spec that proves the old way broken as well.

lib/logstash/codecs/rfc6587.rb
@@ -61,7 +61,8 @@ class LogStash::Codecs::Rfc6587 < LogStash::Codecs::Base
     end
 
     to_read = header.to_i
-    line = data.gets(to_read)
+    line = ""
+    to_read.times { line += data.getc }
 
     if not line
       @leftover = header

spec/codecs/rfc6587_spec.rb
@@ -86,6 +86,15 @@ describe LogStash::Codecs::Rfc6587, :ecs_compatibility_support do
       insist { decoded } == true
     end
 
+    it "should contain correct results when input contains newline" do
+      result = []
+      subject.decode("5 line17 line2\n!5 line3") { |e| result << e }
+      subject.flush { |e| result << e }
+      expect(result.size).to eq(3)
+      expect(result[0].get("message")).to eq("line1")
+      expect(result[1].get("message")).to eq("line2\n!")
+      expect(result[2].get("message")).to eq("line3")
+    end
 
     ecs_compatibility_matrix(:disabled, :v1, :v8 => :v1) do |ecs_select|
 
@@ -137,6 +146,7 @@ describe LogStash::Codecs::Rfc6587, :ecs_compatibility_support do
         expect(result[1].get("message")).to eq("line2")
         expect(result[2].get("message")).to eq("line3")
       end
+
     end
   end
 

commit 0a52c95ab3163cc91b8527c11a244a87689a2549

fix: fix ability to parse interrupted batches
Issue introduced in f480cf9.

refs: f480cf92ce03ce9245981e2e7be29aeb9e7b64a7

lib/logstash/codecs/rfc6587.rb
@@ -62,7 +62,10 @@ class LogStash::Codecs::Rfc6587 < LogStash::Codecs::Base
 
     to_read = header.to_i
     line = ""
-    to_read.times { line += data.getc }
+    to_read.times do
+      break if (c = data.getc) == nil
+      line += c
+    end
 
     if not line
       @leftover = header

logstash-codec-rfc6587.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
 
   s.name            = 'logstash-codec-rfc6587'
-  s.version         = '0.1.3'
+  s.version         = '0.1.4'
   s.licenses        = ['Apache License (2.0)']
   s.summary         = "Reads lines as described in rfc6587"
   s.description     = "This gem is a Logstash plugin required to be installed on top of the Logstash core pipeline using $LS_HOME/bin/logstash-plugin install gemname. This gem is not a stand-alone program"

spec/codecs/rfc6587_spec.rb
@@ -96,6 +96,17 @@ describe LogStash::Codecs::Rfc6587, :ecs_compatibility_support do
       expect(result[2].get("message")).to eq("line3")
     end
 
+    it "should parse partial batches" do
+      result = []
+      subject.decode("5 line15 line")  { |e| result << e }
+      subject.decode("25 line3") { |e| result << e }
+      subject.flush { |e| result << e }
+      expect(result.size).to eq(3)
+      expect(result[0].get("message")).to eq("line1")
+      expect(result[1].get("message")).to eq("line2")
+      expect(result[2].get("message")).to eq("line3")
+    end
+
     ecs_compatibility_matrix(:disabled, :v1, :v8 => :v1) do |ecs_select|
 
       before(:each) do